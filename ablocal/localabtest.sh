e6a753f59e8f04f9f5138aeb416ca5e30e98c2c1
bnuside <bnuside@gmail.com>
2016-08-12 20:09:40 +0800

    specify file path

--- ablocal/localabtest.sh
+++ ablocal/localabtest.sh
@@ -3,13 +3,13 @@ echo "1. kika	2. pro	3. iKey"
 read APP
 if [ "$APP" = "1" ] ; then
 	adb shell pm clear com.qisiemoji.inputmethod
-	adb push koala_event_sampling.properties /sdcard/Android/data/com.qisiemoji.inputmethod/files/koala/koala_event_sampling.properties
+	adb push ~/Documents/koala_event_sampling.properties /sdcard/Android/data/com.qisiemoji.inputmethod/files/koala/koala_event_sampling.properties
 elif [ "$APP" = "2" ] ; then
 	adb shell pm clear com.emoji.coolkeyboard
-	adb push koala_event_sampling.properties /sdcard/Android/data/com.emoji.coolkeyboard/files/koala/koala_event_sampling.properties
+	adb push ~/Documents/koala_event_sampling.properties /sdcard/Android/data/com.emoji.coolkeyboard/files/koala/koala_event_sampling.properties
 elif [ "$APP" = "3" ] ; then
 	adb shell pm clear com.emoji.ikeyboard
-	adb push koala_event_sampling.properties /sdcard/Android/data/com.emoji.ikeyboard/files/koala/koala_event_sampling.properties
+	adb push ~/Document/koala_event_sampling.properties /sdcard/Android/data/com.emoji.ikeyboard/files/koala/koala_event_sampling.properties
 else
 	echo "Wrong Selection, run it again"
 fi
