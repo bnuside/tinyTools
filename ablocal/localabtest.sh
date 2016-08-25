ba45a5a307adfa7247596e06802708ad9900ae7e
bnuside <bnuside@gmail.com>
2016-08-25 20:42:09 +0800

    refactor

--- ablocal/localabtest.sh
+++ ablocal/localabtest.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 echo "1. kika	2. pro	3. iKey"
 read APP
+PKG=""
 if [ "$APP" = "1" ] ; then
-	adb shell pm clear com.qisiemoji.inputmethod
-	adb push ~/Documents/koala_event_sampling.properties /sdcard/Android/data/com.qisiemoji.inputmethod/files/koala/koala_event_sampling.properties
+	PKG="com.qisiemoji.inputmethod"
 elif [ "$APP" = "2" ] ; then
-	adb shell pm clear com.emoji.coolkeyboard
-	adb push ~/Documents/koala_event_sampling.properties /sdcard/Android/data/com.emoji.coolkeyboard/files/koala/koala_event_sampling.properties
+	PKG="com.emoji.coolkeyboard"
 elif [ "$APP" = "3" ] ; then
-	adb shell pm clear com.emoji.ikeyboard
-	adb push ~/Documents/koala_event_sampling.properties /sdcard/Android/data/com.emoji.ikeyboard/files/koala/koala_event_sampling.properties
+	PKG="com.emoji.ikeyboard"
 else
 	echo "Wrong Selection, run it again"
 fi
+
+#clear app data
+COMMON_PREFIX_CLEAR="adb shell pm clear "
+echo `$COMMON_PREFIX_CLEAR$PKG`
+
+
+#push file
+COMMON_PREFIX_PUSH="adb push koala_event_sampling.properties "
+COMMON_MID_PUSH="/sdcard/Android/data/"
+COMMON_TAIL_PUSH="/files/koala/koala_event_sampling.properties"
+echo `$COMMON_PREFIX_PUSH$COMMON_MID_PUSH$PKG$COMMON_TAIL_PUSH`
+
