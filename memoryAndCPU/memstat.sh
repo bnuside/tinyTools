b833e41576773800d182c91e227402774fb6a1d1
bnuside <bnuside@gmail.com>
2016-09-02 15:16:39 +0800

    some tiny modification

---
+++ memoryAndCPU/memstat.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+#PKG=""
+#package(){
+#	if [ $1 = "ikey" ]; then
+#		PKG="com.emoji.ikeyboard"
+#	elif [ $1 = "kika" ]; then
+#		PKG="com.qisiemoji.inputmethod"
+#	elif [ $1 = "pro" ]; then
+#		PKG="com.emoji.coolkeyboard"
+#	else
+#		echo "param error"
+#		exit 0
+#}
+PKG="com.emoji.ikeyboard"
+performance(){
+	#remove old log
+	#rm /data/local/tmp/cpuinfo.csv
+	#rm /data/local/tmp/procrank.csv
+
+	CPUINFO="/data/local/tmp/cpuinfo.csv"
+	PROCRANK="/data/local/tmp/procrank.csv"
+	echo `rm $CPUINFO`
+	echo `rm $PROCRANK`
+	echo "data,time,pid,vss,rss,pss,uss" >> $PROCRANK
+	#dump cpu infomation per second
+	COUNTER=0
+	#36000s means 10 hours
+	TIMES=36000
+	while [ $COUNTER -lt $TIMES ]; do
+		echo $COUNTER
+		date_current=`date '+%F %T'`
+		echo $date_current
+		cpuinfo=`dumpsys cpuinfo | grep $PKG`
+		echo ${COUNTER}${date_current}${cpuinfo} >> $CPUINFO	
+		
+		date_current=`date '+%F %T'`
+		procrank=`Procrank | grep $PKG`
+		echo ${COUNTER}${date_current}${procrank} >> $PROCRANK
+		sleep 1
+		let COUNTER=COUNTER+1
+	done
+
+	chmod 777 *.csv
+}
+
+#THREADS=0
+#while [ $THREADS -lt 2 ]; do
+#	sleep 1
+#	monkey
+#	performance
+#	let THREADS=THREADS+1
+#done&
+performance
---
+++ memoryAndCPU/test.sh
@@ -0,0 +1,4 @@
+CPUINFO="/data/local/tmp/cpuinfo.csv"
+PROCRANK="/data/local/tmp/procrank.csv"
+echo `rm $CPUINFO`
+echo `rm $PROCRANK`
